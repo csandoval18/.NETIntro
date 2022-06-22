56d37aaf3df5f90cc84b18c87e8379ad17478f5e
Christian AS <alvitesscf18@uww.edu>
2022-06-22 15:22:06 -0500

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWordController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+  public class HelloWordController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
