1c5ab435b9f9fae99dc4d55042c3f74c047a3761
Christian AS <alvitesscf18@uww.edu>
2022-06-23 02:52:35 -0500

    commit

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using FirstMVCApp.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+  public class HelloWorldController : Controller
+  {
+    public IActionResult Index()
+    {
+      DogViewModel doggo = new DogViewModel()
+      {
+        Name = "Sif",
+        Age = 2
+      };
+      return View(doggo);
+    }
+
+    public string Hello()
+    {
+      return "Who's there?";
+    }
+
+  }
+}
